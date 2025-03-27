f5157ded458400df2a96cbc5fd7e08e10feba273
Wasim Almadfaa <wm.madfaa@gmail.com>
2025-03-27 11:14:32 +0100

    Add backend and frontend configurations with Deno, including .gitignore files and initial app structure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import deno from "@deno/vite-plugin";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [deno(), react()],
+});
